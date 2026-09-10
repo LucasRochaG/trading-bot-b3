208306879bf7e67a03c61b2ef7699affebe93609
LucasRochaG <lucas.lg.gabriel@gmail.com>
2026-09-10 12:37:30 -0300

    Update db.py

--- engine/persistence/db.py
+++ engine/persistence/db.py
@@ -1,1 +1,23 @@
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from engine.persistence.models import Base
+
+
+def init_db(database_url: str):
+    """
+    Inicializa o banco de dados.
+
+    SQLite é usado no MVP.
+    Futuramente podemos trocar para PostgreSQL/TimescaleDB
+    sem alterar a estrutura principal do robô.
+    """
+
+    engine = create_engine(database_url)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    return Session
